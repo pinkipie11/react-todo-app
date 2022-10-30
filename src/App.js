be6edbd17c3502f7ba98f8bf6277177c04ea3f1a
pinkipie11 <stani.simic22@gmail.com>
2022-10-30 22:49:48 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
-  return <div className="App"></div>;
+  // State stuff
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+  // USE EFFECT - every time todos, or status changes it does hey...
+  useEffect(() => {
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, status]);
+
+  // Functions
+  const filterHandler = () => {
+    switch (status) {
+      case 'completed':
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+
+  //Save to local storage
+  const saveLocalTodos = () => {
+    if (todos.length > 0) {
+      localStorage.setItem('todos', JSON.stringify(todos));
+    }
+  };
+
+  const getLocalTodos = () => {
+    if (localStorage.getItem('todos') === null) {
+      localStorage.setItem('todos', JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem('todos'));
+      setTodos(todoLocal);
+    }
+  };
+  return (
+    <div className="App">
+      <header>
+        <h1>My Todo List</h1>
+      </header>
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        setStatus={setStatus}
+        status={status}
+      />
+      <TodoList
+        filteredTodos={filteredTodos}
+        setTodos={setTodos}
+        todos={todos}
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+function Todo({ text, todo, todos, setTodos }) {
+  // Events
+  const deleteHandler = () => {
+    setTodos(todos.filter((el) => el.id !== todo.id));
+    console.log(todo);
+  };
+  const completeHandler = () => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return {
+            ...item,
+            completed: !item.completed,
+            // switching completed: false/true
+          };
+        }
+        return item;
+      })
+    );
+  };
+
+  return (
+    <div className="todo">
+      {/* complited - we can dynamically update our styling in css */}
+      <li className={`todo-item ${todo.completed ? 'completed' : ''}`}>
+        {text}
+      </li>
+      <button onClick={completeHandler} className="complete-btn">
+        <i className="fas fa-check"></i>
+      </button>
+      <button onClick={deleteHandler} className="trash-btn">
+        <i className="fas fa-trash"></i>
+      </button>
+    </div>
+  );
+}
+
+export default Todo;
